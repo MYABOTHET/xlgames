50743ec9574bc29dca1029833ece87ae294b7b1e
MYABOTHET <myabothet@gmail.com>
2024-12-16 03:49:53 +0300

    Обновление

--- xlgames-backend/xlgames-backend/Program.cs
+++ xlgames-backend/xlgames-backend/Program.cs
@@ -1,6 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<ApplicationDbContext>(
+    options => options.UseNpgsql());
 
 var app = builder.Build();
 
