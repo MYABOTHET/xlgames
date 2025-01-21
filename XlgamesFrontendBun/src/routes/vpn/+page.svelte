40ae371dcf159c6f2d280b386412b4beccf63c23
MYABOTHET <myabothet@gmail.com>
2025-01-21 08:49:29 +0300

    Обновление

--- XlgamesFrontendBun/src/routes/vpn/+page.svelte
+++ XlgamesFrontendBun/src/routes/vpn/+page.svelte
@@ -32,7 +32,8 @@
 </svelte:head>
 
 {#snippet VPNCard(title, price, href)}
-<article class="border-2 border-quaternary rounded-2xl h-[264px] py-6 flex items-center flex-col text-center">
+<article class="border-2 border-quaternary rounded-2xl h-[264px] py-6 flex items-center flex-col text-center
+transition-colors hover:bg-secondary">
   <h1 class="text-2xl font-medium">{title}</h1>
   <p class="mt-6">{price}</p>
   <a class="quinary-block w-fit px-6 py-2.5 mt-auto" {href}>{language.Shared.Order}</a>
@@ -49,9 +50,9 @@
   <section class="grid grid-cols-4 gap-8 max-secondary:grid-cols-3 max-secondary:last:*:grid-cols-subgrid
 max-secondary:last:*:col-start-2 max-ternary:last:*:grid-cols-none max-ternary:last:*:col-start-auto
 max-ternary:grid-cols-2 max-nine:grid-cols-1">
-    {@render VPNCard?.(language.VPN.OneMonth, prices.OneMonth, "/vpn")}
-    {@render VPNCard?.(language.VPN.ThreeMonths, prices.ThreeMonths, "/vpn")}
-    {@render VPNCard?.(language.VPN.HalfYear, prices.HalfYear, "/vpn")}
-    {@render VPNCard?.(language.VPN.Year, prices.Year, "/vpn")}
+    {@render VPNCard?.(language.VPN.OneMonth, prices.OneMonth, "/store/cart.php?a=add&pid=51&billingcycle=montly")}
+    {@render VPNCard?.(language.VPN.ThreeMonths, prices.ThreeMonths, "/store/cart.php?a=add&pid=51&billingcycle=quarterly")}
+    {@render VPNCard?.(language.VPN.HalfYear, prices.HalfYear, "/store/cart.php?a=add&pid=51&billingcycle=semiannually")}
+    {@render VPNCard?.(language.VPN.Year, prices.Year, "/store/cart.php?a=add&pid=51&billingcycle=annually")}
   </section>
 </article>
