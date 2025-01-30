08e51732c9c47cbbca90adeb41d47020059f03be
MYABOTHET <myabothet@gmail.com>
2025-01-31 01:23:51 +0300

    Обновление

--- XlgamesFrontendBun/src/routes/+error.svelte
+++ XlgamesFrontendBun/src/routes/+error.svelte
@@ -1,9 +1,8 @@
 <script>
-  import {page} from '$app/state';
+  import {goto} from "$app/navigation";
+  import {onMount} from "svelte";
+  
+  onMount(async () => {
+    await goto("/");
+  });
 </script>
-
-<article class="flex-center h-full mx-auto">
-  <h1 class="text-(--color-quaternary) text-4xl font-medium mx-auto">
-    {page.error.message} | {page.status}
-  </h1>
-</article>
