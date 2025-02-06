8b11cd28c92230c7799a634ecbdc61632b97e666
MYABOTHET <myabothet@gmail.com>
2025-02-06 19:26:23 +0300

    Обновление

--- XlgamesFrontendBun/src/routes/(public)/dedicated/+page.server.js
+++ XlgamesFrontendBun/src/routes/(public)/dedicated/+page.server.js
@@ -4,7 +4,7 @@ export async function load({fetch}) {
   return {
     servers: (await (await fetch(`${configuration.api}/Products/Servers`)).json()).map(server => {
       try {
-        server.Data = JSON.parse(server.Data);
+        server.Data = JSON.parse(server.Data.replaceAll("&quot;", '"'));
         if (!server.Data) throw new Error();
       } catch {
         server.Data = "";
--- XlgamesFrontendBun/src/routes/(public)/gpu-servers/+page.server.js
+++ XlgamesFrontendBun/src/routes/(public)/gpu-servers/+page.server.js
@@ -6,7 +6,7 @@ export async function load({fetch}) {
   return {
     servers: (await (await fetch(`${configuration.api}/Products/Servers-GPU`)).json()).map(server => {
       try {
-        server.Data = JSON.parse(server.Data);
+        server.Data = JSON.parse(server.Data.replaceAll("&quot;", '"'));
         if (!server.Data) throw new Error();
       } catch {
         server.Data = "";
