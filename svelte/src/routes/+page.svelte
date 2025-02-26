f69e655096f03ec0a2fb0ce9567ec3462d0c5e19
MYABOTHET <myabothet@gmail.com>
2025-02-26 10:14:37 +0300

    Обновление

--- svelte/src/routes/+page.svelte
+++ svelte/src/routes/+page.svelte
@@ -1,5 +1,40 @@
 <script>
-
+  let interfaces = $state([]);
+  let currentInterface = $state({});
+  let inputInterface = $state({});
+  let input = $state("");
+  
+  function selectInterface(item) {
+    currentInterface = item;
+  }
+  
+  function createInput() {
+    inputInterface[input] = "";
+    input = "";
+  }
+  
+  function createInterface() {
+    interfaces.push(inputInterface);
+    inputInterface = {};
+  }
 </script>
 
-Test
+Все интерфейсы
+<br>
+{#each interfaces as item, i}
+  <button style="color: {currentInterface?.name === interfaces[i].name ? 'black' : 'gray'}"
+          onclick={() => selectInterface(interfaces[i])}>{i + 1} + {interfaces[i].name}</button><br>
+{/each}
+
+<br>
+{#each Object.entries(currentInterface) as item}
+  <input bind:value={currentInterface[item[0]]}><br>
+{/each}
+<br>
+<br>
+
+<input placeholder="Имя интерфейса" bind:value={inputInterface.name}><br>
+<button onclick={createInterface}>Создать новый интерфейс</button><br>
+
+<input placeholder="Имя нового поля" bind:value={input}><br>
+<button onclick={createInput}>Создать новое поле</button><br>
