34779a393e26249e8886d1e258b82a90dae7cfc1
MYABOTHET <myabothet@gmail.com>
2025-03-07 23:51:40 +0300

    Init

--- xlgames_whmcs_all/src/App.svelte
+++ xlgames_whmcs_all/src/App.svelte
@@ -1,7 +1,11 @@
 <script>
-
+  import AutomaticPurchaseSelectelProducts from "./modules/AutomaticPurchaseSelectelProducts.svelte";
 </script>
 
 <section id="xlgames_whmcs_all">
-
+  <article class="font-[Roboto] tracking-wide">
+    
+    <AutomaticPurchaseSelectelProducts/>
+  
+  </article>
 </section>
---
+++ xlgames_whmcs_all/tailwind.config.js
@@ -0,0 +1,7 @@
+export default {
+  content: ['./src/**/*.{js,svelte}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
