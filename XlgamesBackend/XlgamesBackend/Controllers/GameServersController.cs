8f2d85e5bf9bb27aac1c3d7581c870726cef63b4
ABOTHET <myabothet@gmail.com>
2025-06-02 19:21:09 +0300

    Обновление

--- XlgamesBackend/XlgamesBackend/Controllers/GameServersController.cs
+++ XlgamesBackend/XlgamesBackend/Controllers/GameServersController.cs
@@ -69,6 +69,7 @@ namespace XlgamesBackend.Controllers
                     Src = gameServer.Src,
                     ControlPanel = gameServer.ControlPanel,
                     FTP = gameServer.FTP,
+                    Execute = gameServer.Execute,
                     DedicatedDescription = gameServer.DedicatedDescription,
                     GameServerDataPrimaryModels = gameServer.GameServerDatas
                         .Select(gameServerData => new GameServerDataPrimaryModel()
